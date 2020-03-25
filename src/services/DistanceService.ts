dcb95ee0b06ec3fca945f89dbcf697fcdc0fa1bd
Scott Banyard <sb14803@my.bristol.ac.uk>
2020-03-25 16:57:04 +0000

    Added geolib library to calculate distances between selected address and parkruns. Top 3 nearest parkruns are highlighted on the map.

--- src/services/DistanceService.ts
+++ src/services/DistanceService.ts
@@ -2,16 +2,15 @@ import { Point, FeatureCollection } from "geojson";
 import { IGeocoderItem } from "../components/Map/types";
 import { getDistance } from "geolib";
 
-const NUM_NEAREST = parseInt(process.env.NUM_NEAREST);
-
+const NUM_NEAREST = parseInt(process.env.NUM_NEAREST, 10);
 export default class DistanceService {
 
-  /**
+  /*
   * Calculate distance between 2 points (accuracy is to 1 meter)
   * @param point1 point A
   * @param point2 point B
   * @return the distance as a number
-  */
+  **/
   static calculateDistance = (point1: Point, point2: Point): number => {
     const point1Coords = { latitude: point1.coordinates[1], longitude: point1.coordinates[0] };
     const points2Coords = { latitude: point2.coordinates[1], longitude: point2.coordinates[0] };
@@ -19,12 +18,12 @@ export default class DistanceService {
     return distance;
   }
 
-  /**
+  /*
   * Calculate the nearest parkruns from an address using a defined distance threshold
   * @param address the selected address from the geocoder
   * @param parkruns the FeatureCollection of parkruns
   * @return a FeatureCollection of parkruns with new properties (distanceToAddress: number and closeParkrun: boolean)
-  */
+  **/
   static getNearestParkruns = (address: IGeocoderItem, parkruns: FeatureCollection): any => {
     parkruns.features.forEach((f: any) => {
       const distance = DistanceService.calculateDistance(address.geometry, f.geometry);
@@ -37,21 +36,21 @@ export default class DistanceService {
     return finalParkruns;
   }
 
-  /**
+  /*
   * Sort the parkruns features in ascending order of distance from address
   * @param parkruns the FeatureCollection of parkruns
   * @return a sorted FeatureCollection of parkruns
-  */
+  **/
   static sortParkruns = (parkruns: FeatureCollection) => {
     parkruns.features.sort((a, b) => (a.properties.distanceToAddress > b.properties.distanceToAddress) ? 1 : -1);
     return parkruns;
   }
 
-  /**
+  /*
   * Identify the top three parkruns
   * @param sortedParkruns a sorted FeatureCollection of parkruns
   * @return a modified FeatureCollection where the property 'closeParkrun' = true for the nearest parkruns
-  */
+  **/
   static identifyTop3Parkruns = (sortedParkruns: FeatureCollection) => {
     for (let i = 0; i < NUM_NEAREST; i++) {
       sortedParkruns.features[i].properties.closeParkrun = "true";
