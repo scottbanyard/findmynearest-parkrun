075d2a4b5fdd1ec41ae9f879cee5687744c490e3
scottbanyard <scottbanyard1@yahoo.co.uk>
2023-03-25 14:34:07 +0000

    Updated webpack-dev-server command

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,43 @@
-const path = require("path");
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: './src/index.tsx',
   output: {
-    filename: "bundle.js",
-    path: path.join(__dirname, "dist/")
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist/')
   },
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json", "jpg", ".css"]
+    extensions: ['.ts', '.tsx', '.js', '.json', 'jpg', '.css']
   },
 
   devServer: {
-    contentBase: path.join("./dist"),
-    publicPath: "/"
+    contentBase: path.join('./dist'),
+    publicPath: '/',
+    hot: true,
+    open: true
   },
 
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      { test: /\.css$/i, use: [ { loader: 'style-loader' }, { loader: 'css-loader' } ] },
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.css$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      },
       {
         test: /\.(gif|png|jpe?g|svg)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "img/",
-              publicPath: "/"
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: '/'
             }
           }
         ]
@@ -43,8 +48,8 @@ module.exports = {
   plugins: [
     new Dotenv(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      favicon: "./src/images/icon.ico"
+      template: './src/index.html',
+      favicon: './src/images/icon.ico'
     })
   ]
 };
